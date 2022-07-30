919fa88695114d01ec5007d813612f85ea3c0a47
karenL26 <karen.lima@estudiantes.utec.edu.uy>
2022-07-30 22:04:16 +0000

    EDA project

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Template for Machine Learning projects
+# Data Preprocessing Project
+
+In this project I work with the New York Airbnb data from Kaggle.com This data was downloaded from this [link](https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv).
+
+[Here](src/explore.ipynb) you can find the exploratory data analysis maded to find patterns and get insights from the data.
+
+And [here](src/app.py) you can find the final cleaning process, where the the pipeline that cleans the data is created.
+
+## About the cleaning process:
+
+The goal was to obtain a pipeline that cleans the data, in order to use it for future price prediction.
+To obtain this pipeline, the following was done:
+* The irrelevant columns were dropped.
+* In the column reviews_per_month the missing values were replaced with 0.0
+* The categorical variables were codificated
+* The listing with price least or equal tha 0 were removed.
+
+Finally the pipeline was saved in cvs file.
--- src/app.py
+++ src/app.py
@@ -1,4 +1,25 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
 
-# your code here
+# Reading de data
+df_raw = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
+
+#Creating a copy to clean de data
+df_clean = df_raw.copy()
+
+print("Droping the irrelevant columns")
+df_clean.drop(['id','host_name', 'name', 'last_review'], axis=1, inplace=True)
+
+print("Replacing the null values in reviews_per_month with 0.0")
+df_clean['reviews_per_month'].fillna(0.0, inplace=True)
+
+print("Codification of categorical variables")
+# Encoding categorical data
+df_clean = pd.get_dummies(df_clean, columns=['room_type'])
+df_clean = pd.get_dummies(df_clean, columns=['neighbourhood_group'])
+df_clean = pd.get_dummies(df_clean, columns=['neighbourhood'])
+
+print("Droping listings with prices least or equal than 0")
+df_clean = df_clean.loc[df_clean['price'] > 0]
+
+print("Saving the pipeline in csv file")
+df_clean.to_csv('data/processed/data_processed.csv')
